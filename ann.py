e9d394044a9dc8bbc173e7a20814bcf51f2f9988
Sandesh Tiwari <sandeshtiwari@live.com>
2019-01-13 14:40:22 -0600

    feat: Encoding categorical data

--- ann.py
+++ ann.py
@@ -12,3 +12,20 @@ import pandas as pd
 dataset = pd.read_csv('Churn_Modelling.csv')
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, -1].values
+
+
+# Encoding the categorical variables
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelencoder_X_1 = LabelEncoder()
+# Encoding the 'country' column to numbers
+X[:,1] = labelencoder_X_1.fit_transform(X[:,1])
+
+labelencoder_X_2 = LabelEncoder()
+# Encoding the 'gender' column to numbers
+X[:,2] = labelencoder_X_2.fit_transform(X[:,2])
+
+# One-hot-encoder for 'country' column
+onehotencoder = OneHotEncoder(categorical_features=[1])
+X = onehotencoder.fit_transform(X).toarray()
+
