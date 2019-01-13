a09963fdd3c6f7d44f08442f6c35fd1f43c48906
Sandesh Tiwari <sandeshtiwari@live.com>
2019-01-13 14:33:05 -0600

    feat: Importing the dataset

--- ann.py
+++ ann.py
@@ -8,3 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, -1].values
