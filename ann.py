c3ab8bf15068ad3f0614aba808aff20afcac90f9
Sandesh Tiwari <sandeshtiwari@live.com>
2019-01-13 14:58:27 -0600

    feat: Scale the inputs to be the same

--- ann.py
+++ ann.py
@@ -34,3 +34,9 @@ X = X[:,1:]
 # Splitting the dataset
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=0)
+
+# Features scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
